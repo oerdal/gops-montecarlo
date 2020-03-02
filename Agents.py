e6d11ddf7477dc1074a14014320d450b65ca529f
jack_zhang <yuez29@uw.edu>
2020-03-01 16:55:21 -0800

    Updated bracket agent

--- Agents.py
+++ Agents.py
@@ -30,3 +30,24 @@ class RandomAgent(Agent):
     def next_move(self, game_state, prize, leftover_prize=None):
         card = self.plays.pop()
         return card
+
+class BracketAgent(Agent):
+    # we don't need any additional init
+    def next_move(self, game_state, prize, leftover_prize=None):
+        plays = {
+            1: 2,
+            2: 3,
+            3: 1,
+            4: 5,
+            5: 6,
+            6: 4,
+            7: 8,
+            8: 9,
+            9: 7,
+            10: 11,
+            11: 12,
+            12: 10,
+            13: 13
+        }
+        return plays[prize]
+    
